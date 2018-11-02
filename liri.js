2e4858a633456c7ab45ce996c3c87b475dee2b95
Duncan Dam <duncandam@duncans-mbp.lan>
2018-11-02 16:06:30 -0500

    all files

---
+++ liri.js
@@ -0,0 +1,123 @@
+// Load packages
+let NodeSpotify = require("node-spotify-api");
+let request = require("request");
+let moment = require("moment");
+let fs = require("fs");
+let dotenv = require("dotenv").config();
+let keys = require("./keys.js");
+
+//store arguments
+let command = process.argv[2];
+let nodeArgs = process.argv;
+
+
+//string movie, song or bandname
+let searchName = nodeArgs.slice(3).join("+");
+// console.log(searchName);
+
+//create function to search for movie
+function omdbMovie(movie) {
+    var omdbURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+    request(omdbURL, function (error, response, body) {
+        // console.log(response)
+        if (!error && response.statusCode == 200) {
+            var body = JSON.parse(body);
+            //console.log(body);
+            console.log("Title: " + body.Title);
+            console.log("Release Year: " + body.Year);
+            console.log("IMdB Rating: " + body.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+            console.log("Country: " + body.Country);
+            console.log("Language: " + body.Language);
+            console.log("Plot: " + body.Plot);
+            console.log("Actors: " + body.Actors);
+        }
+
+        else {
+            console.log('Error occurred.')
+        }
+    })
+}
+if (command === "movie-this") {
+    if (searchName) {
+        omdbMovie(searchName);
+    }
+    if (command === " ") {
+        console.log("----------------------------");
+        console.log("If you haven't watched Mr. Nobody, then you should: <http://www.imdb.com/title/tt0485947/>");
+        console.log("It's on Netflix");
+        console.log("----------------------------");
+        omdbMovie("Mr.Nobody");
+    }
+}
+
+//Create function to search for band in town
+function bandInTown(artist) {
+    var bandInTownURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    request(bandInTownURL, function (error, response, body) {
+        //console.log (response)
+        if (!error && response.statusCode == 200) {
+            var body = JSON.parse(body);
+            //console.log (body)
+            for (let i = 0; i < body.length; i++) {
+                console.log("Name of Venue: " + body[i].venue.name);
+                console.log("Venue Location: " + body[i].venue.city + ", " + body[i].venue.region + ", " + body[i].venue.country);
+                console.log("Date of the Event: " + moment(body[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a"));
+            }
+        }
+    })
+}
+if (command === "concert-this") {
+    if (searchName) {
+        bandInTown(searchName);
+    }
+}
+
+
+//NOT WORKING YET
+//Create function to search for spotify song
+function searchSpotifySong(song) {
+    var spotify = new Spotify(keys.spotify)
+
+    console.log(spotify);
+
+    spotify.search({ type: 'track', query: song }, function (err, data) {
+        console.log(data)
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+    });
+}
+
+if (command === "spotify-this-song") {
+    if (searchName) {
+        searchSpotifySong(searchName);
+    }
+    else {
+        searchSpotifySong("you bring me joy")
+    }
+}
+
+//NOT SURE WHAT IS GOING ON HERE
+//create function for doWhatItSay 
+function doWhatitSay() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        //console.log(data);
+        let random = data.split(",")
+        // console.log(random);
+        // console.log(random[0]);
+        // console.log(random[1]);
+
+        bandInTown(random[1])
+    });
+}
+if (command === "do-what-it-says") {
+    doWhatitSay();
+}
+
