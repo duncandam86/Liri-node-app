87d45b150bf8379b5d50595140907fc0b0f020a9
Duncan Dam <duncandam@duncans-mbp.fsm.northwestern.edu>
2018-11-06 10:36:10 -0600

    Update JS with ES6 notation

--- liri.js
+++ liri.js
@@ -25,54 +25,9 @@ function omdbMovie(movie) {
         if (!error && response.statusCode == 200) {
             var body = JSON.parse(body);
             //console.log(body);
-            console.log("----------------------------");
-            console.log("Title: " + body.Title);
-            console.log("Release Year: " + body.Year);
-            console.log("IMdB Rating: " + body.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
-            console.log("Country: " + body.Country);
-            console.log("Language: " + body.Language);
-            console.log("Plot: " + body.Plot);
-            console.log("Actors: " + body.Actors);
-            console.log("----------------------------");
-
+            console.log(`----------------------------\nTitle: ${body.Title}\nRelease Year: ${body.Year}\nIMdB Rating: ${body.imdbRating}\nRotten Tomatoes Rating: ${body.tomatoRating}\nCountry: ${body.Country}\nLanguage: ${body.Language}\nPlot: ${body.Plot}\nActors: ${body.Actors}\n----------------------------`);
             //added into log.txt
-            fs.appendFile("log.txt", "Title: " + body.Title, function (err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-            fs.appendFile("log.txt", ". Release Year: " + body.Year, function (err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-            fs.appendFile("log.txt", ". IMdB Rating: " + body.imdbRating, function (err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-            fs.appendFile("log.txt", ". Rotten Tomatoes Rating: " + body.tomatoRating, function (err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-            fs.appendFile("log.txt", ". Country: " + body.Country, function (err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-            fs.appendFile("log.txt", ". Language: " + body.Language, function (err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-            fs.appendFile("log.txt", ". Plot: " + body.Plot, function (err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-            fs.appendFile("log.txt", ". Actors: " + body.Actors, function (err) {
+            fs.appendFile("log.txt", `----------------------------\nTitle: ${body.Title}\nRelease Year: ${body.Year}\nIMdB Rating: ${body.imdbRating}\nRotten Tomatoes Rating: ${body.tomatoRating}\nCountry: ${body.Country}\nLanguage: ${body.Language}\nPlot: ${body.Plot}\nActors: ${body.Actors}\n----------------------------`, function (err) {
                 if (err) {
                     console.log(err);
                 }
@@ -90,10 +45,7 @@ if (command === "movie-this") {
         omdbMovie(searchName);
     }
     if (searchName === "") {
-        console.log("----------------------------");
-        console.log("If you haven't watched Mr. Nobody, then you should: <http://www.imdb.com/title/tt0485947/>");
-        console.log("It's on Netflix");
-        console.log("----------------------------");
+        console.log(`----------------------------\nIf you haven't watched Mr. Nobody, then you should: <http://www.imdb.com/title/tt0485947/>\nIt's on Netflix\n----------------------------`);
         omdbMovie("Mr.Nobody");
     }
 }
@@ -108,23 +60,10 @@ function bandInTown(artist) {
             var body = JSON.parse(body);
             //console.log (body)
             for (let i = 0; i < body.length; i++) {
-                console.log("----------------------------");
-                console.log("Name of Venue: " + body[i].venue.name);
-                console.log("Venue Location: " + body[i].venue.city + ", " + body[i].venue.region + ", " + body[i].venue.country);
-                console.log("Date of the Event: " + moment(body[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a"));
-                console.log("----------------------------");
+                console.log(`----------------------------\nName of Venue: ${body[i].venue.name}\nVenue Location: ${body[i].venue.city}, ${body[i].venue.region}, ${body[i].venue.country}\nDate of the Event: ${moment(body[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a")}\n----------------------------`);
 
-                fs.appendFile("log.txt", `Name of Venue: ${body[i].venue.name}`, function (err) {
-                    if (err) {
-                        console.log(err);
-                    }
-                });
-                fs.appendFile("log.txt", `. Venue Location: ${body[i].venue.city}, ${body[i].venue.region}, ${body[i].venue.country}`, function (err) {
-                    if (err) {
-                        console.log(err);
-                    }
-                });
-                fs.appendFile("log.txt", `. Date of the Event: ${moment(body[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a")}`, function (err) {
+                //add to log.txt
+                fs.appendFile("log.txt", `----------------------------\nName of Venue: ${body[i].venue.name}\nVenue Location: ${body[i].venue.city}, ${body[i].venue.region}, ${body[i].venue.country}\nDate of the Event: ${moment(body[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a")}\n----------------------------`, function (err) {
                     if (err) {
                         console.log(err);
                     }
@@ -147,38 +86,14 @@ function searchSpotifySong(song) {
     // console.log(spotify);
 
     spotify.search({ type: 'track', query: song }, function (err, data) {
-        //console.log(data);
-        //console.log(data.tracks);
-        //console.log(data.tracks.items[0]);
-        //console.log(data.tracks.items[0].album);
-        //console.log(data.tracks.items[0].album.artists);
         if (err) {
             return console.log('Error occurred: ' + err);
         }
         else {
-            console.log("----------------------------");
-            console.log("Artist: " + data.tracks.items[0].artists[0].name);
-            console.log("The song's name: " + data.tracks.items[0].name);
-            console.log("Preview link of the song: " + data.tracks.items[0].preview_url);
-            console.log("Album: " + data.tracks.items[0].album.name);
-            console.log("----------------------------");
+            console.log(`----------------------------\nArtist: ${data.tracks.items[0].artists[0].name}\nThe song's name: ${data.tracks.items[0].name}\nPreview link of the song: ${data.tracks.items[0].preview_url}\nAlbum: ${data.tracks.items[0].album.name}\n----------------------------`);
+            
             //Append these data into a log.txt file
-            fs.appendFile("log.txt", `Artist:  ${data.tracks.items[0].artists[0].name}`, function (err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-            fs.appendFile("log.txt", `. The song's name:  ${data.tracks.items[0].name}`, function (err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-            fs.appendFile("log.txt", `. Preview link of the song:  ${data.tracks.items[0].preview_url}`, function (err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-            fs.appendFile("log.txt", `. Album:  ${data.tracks.items[0].album.name}`, function (err) {
+            fs.appendFile("log.txt", `----------------------------\nArtist: ${data.tracks.items[0].artists[0].name}\nThe song's name: ${data.tracks.items[0].name}\nPreview link of the song: ${data.tracks.items[0].preview_url}\nAlbum: ${data.tracks.items[0].album.name}\n----------------------------`, function (err) {
                 if (err) {
                     console.log(err);
                 }
